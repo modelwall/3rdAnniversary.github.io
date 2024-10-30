443b0928c30e732dbb7daec077f167866e523158
Nattakan Kaewpairam <75587734+modelwall@users.noreply.github.com>
2024-10-30 14:47:43 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //  output: 'export',
-    //  images: {
-    //      unoptimized: true,
-    //  },
-    //  basePath: '/3rdAnniversary.github.io',
-    //  assetPrefix: '/3rdAnniversary.github.io/',
-    //  distDir: 'out', // Add this line
+      output: 'export',
+      images: {
+          unoptimized: true,
+      },
+      basePath: '/3rdAnniversary.github.io',
+      assetPrefix: '/3rdAnniversary.github.io/',
+      distDir: 'out', // Add this line
 };
 
 export default nextConfig;
