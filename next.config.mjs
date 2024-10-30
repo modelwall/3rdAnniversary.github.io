8cbd0428410ed045c2050b630ed5b05c46fc53e7
Nattakan Kaewpairam <Nattakan.ka@rxnet.orange>
2024-10-30 14:43:43 +0700

    fix merge conflict

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,13 @@
-const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     webpack: (config) => {
-         config.resolve.alias['@'] = path.join(__dirname, 'src')
-         return config
-     },
-     output: 'export',
-     images: {
-         unoptimized: true,
-     },
-     basePath: '/3rdAnniversary.github.io',
-     assetPrefix: '/3rdAnniversary.github.io/',
-     distDir: 'out', // Add this line
+    //  output: 'export',
+    //  images: {
+    //      unoptimized: true,
+    //  },
+    //  basePath: '/3rdAnniversary.github.io',
+    //  assetPrefix: '/3rdAnniversary.github.io/',
+    //  distDir: 'out', // Add this line
 };
 
 export default nextConfig;
